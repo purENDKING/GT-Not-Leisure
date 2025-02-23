d117c8dc218475c18d7ff2a18c001263377e026b
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-02-23 23:52:41 +0800

    添加中子晶圆的用途

--- src/main/java/com/science/gtnl/common/recipe/GregTech/LaserEngraverRecipes.java
+++ src/main/java/com/science/gtnl/common/recipe/GregTech/LaserEngraverRecipes.java
@@ -1,16 +1,41 @@
 package com.science.gtnl.common.recipe.GregTech;
 
+import net.minecraft.item.ItemStack;
+
 import com.science.gtnl.common.GTNLItemList;
 import com.science.gtnl.common.recipe.IRecipePool;
 
 import gregtech.api.enums.GTValues;
 import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.enums.TierEU;
 import gregtech.api.recipe.RecipeMap;
 import gregtech.api.recipe.RecipeMaps;
+import gregtech.api.util.GTOreDictUnificator;
+import gregtech.api.util.GTUtility;
 
 public class LaserEngraverRecipes implements IRecipePool {
 
     final RecipeMap<?> lER = RecipeMaps.laserEngraverRecipes;
+
+    public void recipeWithPurifiedWater(ItemStack[] inputs, ItemStack[] outputs, Materials lowTierWater,
+        Materials highTierWater, int duration, int boostedDuration, long eut) {
+        GTValues.RA.stdBuilder()
+            .itemInputs(inputs)
+            .itemOutputs(outputs)
+            .fluidInputs(lowTierWater.getFluid(100L))
+            .duration(duration)
+            .eut(eut)
+            .addTo(lER);
+        GTValues.RA.stdBuilder()
+            .itemInputs(inputs)
+            .itemOutputs(outputs)
+            .fluidInputs(highTierWater.getFluid(100L))
+            .duration(boostedDuration)
+            .eut(eut)
+            .addTo(lER);
+    }
 
     @Override
     public void loadRecipes() {
@@ -23,6 +48,108 @@ public class LaserEngraverRecipes implements IRecipePool {
             .duration(200)
             .eut(7864320)
             .addTo(lER);
+
+        GTValues.RA.stdBuilder()
+            .itemInputs(
+                GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.EnderPearl, 1L)))
+            .itemOutputs(GTUtility.copyAmountUnsafe(64, ItemList.Circuit_Wafer_NAND.get(1)))
+            .fluidInputs(Materials.Grade5PurifiedWater.getFluid(100))
+            .specialValue(0)
+            .noOptimize()
+            .duration(200)
+            .eut(TierEU.RECIPE_IV)
+            .addTo(lER);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.EnderPearl, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(64, ItemList.Circuit_Wafer_NAND.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            200,
+            100,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.Diamond, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(256, ItemList.Circuit_Wafer_CPU.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            50,
+            25,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.Emerald, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(16, ItemList.Circuit_Wafer_SoC2.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            500,
+            250,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.GarnetYellow, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(64, ItemList.Circuit_Wafer_SoC.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            500,
+            250,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.GarnetYellow, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(256, ItemList.Circuit_Wafer_ULPIC.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            50,
+            25,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.Topaz, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(256, ItemList.Circuit_Wafer_Simple_SoC.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            50,
+            25,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.Sapphire, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(64, ItemList.Circuit_Wafer_PIC.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            200,
+            100,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.GreenSapphire, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(256, ItemList.Circuit_Wafer_Ram.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            50,
+            25,
+            TierEU.RECIPE_IV);
+
+        recipeWithPurifiedWater(
+            new ItemStack[] { GTNLItemList.NeutroniumWafer.get(1),
+                GTUtility.copyAmountUnsafe(0, GTOreDictUnificator.get(OrePrefixes.lens, Materials.EnderEye, 1L)) },
+            new ItemStack[] { GTUtility.copyAmountUnsafe(64, ItemList.Circuit_Wafer_NOR.get(1)) },
+            Materials.Grade5PurifiedWater,
+            Materials.Grade6PurifiedWater,
+            200,
+            100,
+            TierEU.RECIPE_IV);
 
     }
 }
