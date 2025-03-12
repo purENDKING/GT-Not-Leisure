e94dcb3ed6dc122736779c103c4d62905a5aa6cf
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-03-13 00:19:32 +0800

    微调玩偶的代码

--- src/main/java/com/science/gtnl/common/block/blocks/playerDoll/BlockPlayerDollRenderer.java
+++ src/main/java/com/science/gtnl/common/block/blocks/playerDoll/BlockPlayerDollRenderer.java
@@ -35,103 +35,101 @@ public class BlockPlayerDollRenderer extends TileEntitySpecialRenderer {
 
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float partialTicks) {
-        if (!(tileEntity instanceof TileEntityPlayerDoll)) {
-            return; // 如果不是 TileEntityPlayerDoll，直接返回
-        }
+        if (tileEntity instanceof TileEntityPlayerDoll tileEntityPlayerDoll) {
 
-        Minecraft minecraft = Minecraft.getMinecraft();
-        GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y, z + 0.5);
+            Minecraft minecraft = Minecraft.getMinecraft();
+            GL11.glPushMatrix();
+            GL11.glTranslated(x + 0.5, y, z + 0.5);
 
-        // 处理方块朝向
-        int orientation = tileEntity.getBlockMetadata();
-        switch (orientation) {
-            case 4:
-                GL11.glRotatef(90, 0, 1, 0);
-                break;
-            case 5:
-                GL11.glRotatef(-90, 0, 1, 0);
-                break;
-            case 3:
-                GL11.glRotatef(180, 0, 1, 0);
-                break;
-            default:
-                break;
-        }
-        GL11.glRotatef(180, 0, 1, 0); // 默认旋转
-
-        // 初始化纹理
-        ResourceLocation skinTexture = DEFAULT_SKIN;
-        ResourceLocation capeTexture = DEFAULT_CAPE;
-
-        if (MainConfig.enableCustomPlayerDoll) {
-            GameProfile skullOwner = null;
-
-            // 获取 TileEntity 的 NBT 数据
-            NBTTagCompound nbt = new NBTTagCompound();
-            tileEntity.writeToNBT(nbt); // 将 TileEntity 数据写入 NBT
-
-            if (nbt.hasKey("SkullOwner", 8)) { // 8 表示 NBTTagString
-                // SkullOwner 是字符串，直接获取玩家名称
-                String playerName = nbt.getString("SkullOwner");
-                skullOwner = new GameProfile(null, playerName);
-            } else if (nbt.hasKey("SkullOwner", 10)) { // 10 表示 NBTTagCompound
-                // SkullOwner 是复合标签，使用 NBTUtil 解析 GameProfile
-                NBTTagCompound ownerTag = nbt.getCompoundTag("SkullOwner");
-                skullOwner = NBTUtil.func_152459_a(ownerTag);
+            // 处理方块朝向
+            int orientation = tileEntity.getBlockMetadata();
+            switch (orientation) {
+                case 4:
+                    GL11.glRotatef(90, 0, 1, 0);
+                    break;
+                case 5:
+                    GL11.glRotatef(-90, 0, 1, 0);
+                    break;
+                case 3:
+                    GL11.glRotatef(180, 0, 1, 0);
+                    break;
+                default:
+                    break;
             }
+            GL11.glRotatef(180, 0, 1, 0); // 默认旋转
 
-            if (skullOwner != null) {
-                skullOwner = getGameProfile(skullOwner); // 获取完整的 GameProfile
-            } else {
-                // 如果没有 SkullOwner 数据，使用默认玩家（当前玩家）
-                String playerName = Minecraft.getMinecraft().thePlayer.getCommandSenderName();
-                skullOwner = new GameProfile(null, playerName);
-                skullOwner = getGameProfile(skullOwner); // 获取完整的 GameProfile
-            }
+            // 初始化纹理
+            ResourceLocation skinTexture = DEFAULT_SKIN;
+            ResourceLocation capeTexture = DEFAULT_CAPE;
 
-            // 如果 GameProfile 有效，加载皮肤和披风纹理
-            if (skullOwner != null) {
-                Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> textureMap = minecraft.func_152342_ad()
-                    .func_152788_a(skullOwner);
+            if (MainConfig.enableCustomPlayerDoll) {
+                // 获取 TileEntity 的 NBT 数据
+                NBTTagCompound nbt = new NBTTagCompound();
+                tileEntity.writeToNBT(nbt); // 将 TileEntity 数据写入 NBT
+                GameProfile gameprofile = tileEntityPlayerDoll.getSkullOwner();
 
-                if (textureMap.containsKey(MinecraftProfileTexture.Type.SKIN)) {
-                    skinTexture = minecraft.func_152342_ad()
-                        .func_152792_a(
-                            textureMap.get(MinecraftProfileTexture.Type.SKIN),
-                            MinecraftProfileTexture.Type.SKIN);
+                if (nbt.hasKey("SkullOwner", 8)) { // 8 表示 NBTTagString
+                    // SkullOwner 是字符串，直接获取玩家名称
+                    String playerName = nbt.getString("SkullOwner");
+                    gameprofile = new GameProfile(null, playerName);
+                } else if (nbt.hasKey("SkullOwner", 10)) { // 10 表示 NBTTagCompound
+                    // SkullOwner 是复合标签，使用 NBTUtil 解析 GameProfile
+                    NBTTagCompound ownerTag = nbt.getCompoundTag("SkullOwner");
+                    gameprofile = NBTUtil.func_152459_a(ownerTag);
                 }
-                if (textureMap.containsKey(MinecraftProfileTexture.Type.CAPE)) {
-                    capeTexture = minecraft.func_152342_ad()
-                        .func_152792_a(
-                            textureMap.get(MinecraftProfileTexture.Type.CAPE),
-                            MinecraftProfileTexture.Type.CAPE);
+
+                if (gameprofile != null) {
+                    gameprofile = getGameProfile(gameprofile); // 获取完整的 GameProfile
+                } else {
+                    // 如果没有 SkullOwner 数据，使用默认玩家（当前玩家）
+                    String playerName = Minecraft.getMinecraft().thePlayer.getCommandSenderName();
+                    gameprofile = new GameProfile(null, playerName);
+                    gameprofile = getGameProfile(gameprofile); // 获取完整的 GameProfile
+                }
+
+                // 如果 GameProfile 有效，加载皮肤和披风纹理
+                if (gameprofile != null) {
+                    Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> textureMap = minecraft.func_152342_ad()
+                        .func_152788_a(gameprofile);
+
+                    if (textureMap.containsKey(MinecraftProfileTexture.Type.SKIN)) {
+                        skinTexture = minecraft.func_152342_ad()
+                            .func_152792_a(
+                                textureMap.get(MinecraftProfileTexture.Type.SKIN),
+                                MinecraftProfileTexture.Type.SKIN);
+                    }
+                    if (textureMap.containsKey(MinecraftProfileTexture.Type.CAPE)) {
+                        capeTexture = minecraft.func_152342_ad()
+                            .func_152792_a(
+                                textureMap.get(MinecraftProfileTexture.Type.CAPE),
+                                MinecraftProfileTexture.Type.CAPE);
+                    }
                 }
             }
+
+            // 绑定皮肤纹理并渲染模型
+            Minecraft.getMinecraft().renderEngine.bindTexture(skinTexture);
+            renderModelParts(
+                "Body",
+                "Body_Layer",
+                "Hat",
+                "Hat_Layer",
+                "Head",
+                "Left_Arm",
+                "Left_Arm_Layer",
+                "Left_Leg",
+                "Left_Leg_Layer",
+                "Right_Arm",
+                "Right_Arm_Layer",
+                "Right_Leg",
+                "Right_Leg_Layer");
+
+            // 绑定披风纹理并渲染披风部分
+            Minecraft.getMinecraft().renderEngine.bindTexture(capeTexture);
+            modelCustom.renderPart("cape");
+
+            GL11.glPopMatrix();
         }
-
-        // 绑定皮肤纹理并渲染模型
-        Minecraft.getMinecraft().renderEngine.bindTexture(skinTexture);
-        renderModelParts(
-            "Body",
-            "Body_Layer",
-            "Hat",
-            "Hat_Layer",
-            "Head",
-            "Left_Arm",
-            "Left_Arm_Layer",
-            "Left_Leg",
-            "Left_Leg_Layer",
-            "Right_Arm",
-            "Right_Arm_Layer",
-            "Right_Leg",
-            "Right_Leg_Layer");
-
-        // 绑定披风纹理并渲染披风部分
-        Minecraft.getMinecraft().renderEngine.bindTexture(capeTexture);
-        modelCustom.renderPart("cape");
-
-        GL11.glPopMatrix();
     }
 
     private GameProfile getGameProfile(GameProfile profile) {
--- src/main/java/com/science/gtnl/common/block/blocks/playerDoll/ItemPlayerDoll.java
+++ src/main/java/com/science/gtnl/common/block/blocks/playerDoll/ItemPlayerDoll.java
@@ -4,12 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTUtil;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-
-import com.mojang.authlib.GameProfile;
 
 public class ItemPlayerDoll extends ItemBlock {
 
@@ -20,43 +15,6 @@ public class ItemPlayerDoll extends ItemBlock {
     @Override
     public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
         float hitX, float hitY, float hitZ, int metadata) {
-        // 调用父类方法放置方块
-        boolean placed = super.placeBlockAt(stack, player, world, x, y, z, side, hitX, hitY, hitZ, metadata);
-
-        if (placed) {
-            // 获取放置方块的 TileEntity
-            TileEntity tileEntity = world.getTileEntity(x, y, z);
-
-            if (tileEntity instanceof TileEntityPlayerDoll tileEntityPlayerDoll) {
-                // 检查 ItemStack 是否包含 SkullOwner 的 NBT 数据
-                if (stack.hasTagCompound()) {
-                    NBTTagCompound nbt = stack.getTagCompound();
-                    GameProfile profile = null;
-
-                    if (nbt.hasKey("SkullOwner", 8)) { // 8 表示 NBTTagString
-                        // SkullOwner 是字符串，直接获取玩家名称
-                        String playerName = nbt.getString("SkullOwner");
-                        profile = new GameProfile(null, playerName);
-                    } else if (nbt.hasKey("SkullOwner", 10)) { // 10 表示 NBTTagCompound
-                        // SkullOwner 是复合标签，使用 NBTUtil 解析 GameProfile
-                        NBTTagCompound ownerTag = nbt.getCompoundTag("SkullOwner");
-                        profile = NBTUtil.func_152459_a(ownerTag);
-                    }
-
-                    if (profile != null) {
-                        ((TileEntityPlayerDoll) tileEntity).getGameProfile(profile);
-                    } else {
-                        // 如果没有 SkullOwner 数据，则设置为放置方块的玩家
-                        String playerName = player.getCommandSenderName(); // 获取放置方块的玩家名字
-                        profile = new GameProfile(null, playerName); // 创建 GameProfile
-                        tileEntityPlayerDoll.getGameProfile(profile); // 调用 getGameProfile
-                    }
-                } else {
-                    ((TileEntityPlayerDoll) tileEntity).getGameProfileNull();
-                }
-            }
-        }
-
-        return placed;
+        return super.placeBlockAt(stack, player, world, x, y, z, side, hitX, hitY, hitZ, metadata);
     }
 }
