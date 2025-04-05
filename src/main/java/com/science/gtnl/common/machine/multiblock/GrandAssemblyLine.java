66be9ee1e9ade6d93985754b0960935e6b69d565
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-04-05 11:19:40 +0800

    添加装配线的配方检查兼容

--- src/main/java/com/science/gtnl/common/machine/multiblock/GrandAssemblyLine.java
+++ src/main/java/com/science/gtnl/common/machine/multiblock/GrandAssemblyLine.java
@@ -184,7 +184,7 @@ public class GrandAssemblyLine extends MTEExtendedPowerMultiBlockBase<GrandAssem
         // 第一步：初始化参数
         ItemStack controllerItem = getControllerSlot();
         this.ParallelTier = getParallelTier(controllerItem);
-        int limit = 20; // limit 取批处理时间 20 tick
+        int limit = 20;
         long energyEU = GTValues.VP[energyHatchTier] * (useSingleAmp ? 1 : getMaxInputAmps() / 4); // 能源仓最大输入功率
         int maxParallel = getMaxParallelRecipes(); // 最大并行数
 
@@ -248,7 +248,8 @@ public class GrandAssemblyLine extends MTEExtendedPowerMultiBlockBase<GrandAssem
                 AssemblyLineUtils.LookupResult tLookupResult = AssemblyLineUtils
                     .findAssemblyLineRecipeFromDataStick(tDataStick, false);
 
-                if (tLookupResult.getType() == AssemblyLineUtils.LookupResultType.VALID_STACK_AND_VALID_HASH) {
+                if (tLookupResult.getType() == AssemblyLineUtils.LookupResultType.VALID_STACK_AND_VALID_HASH
+                    || tLookupResult.getType() == AssemblyLineUtils.LookupResultType.VALID_STACK_AND_VALID_RECIPE) {
                     GTRecipe.RecipeAssemblyLine tRecipe = tLookupResult.getRecipe();
                     if (tRecipe != null) {
                         // 处理 mInputs 和 mOreDictAlt
