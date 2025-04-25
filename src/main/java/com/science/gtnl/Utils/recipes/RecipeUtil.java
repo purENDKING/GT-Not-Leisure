2a02ff488053ffd8282a16811ac3936f96bb4efe
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-04-25 16:05:35 +0800

    spotlessApply

--- src/main/java/com/science/gtnl/Utils/recipes/RecipeUtil.java
+++ src/main/java/com/science/gtnl/Utils/recipes/RecipeUtil.java
@@ -1,6 +1,10 @@
 package com.science.gtnl.Utils.recipes;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import net.minecraft.item.ItemStack;
