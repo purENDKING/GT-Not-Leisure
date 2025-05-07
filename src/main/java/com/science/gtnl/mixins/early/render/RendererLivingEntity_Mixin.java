6eb82efbd3b1ac31fdddf4f9ad48f500dc0a0332
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-05-07 22:36:05 +0800

    Create RendererLivingEntity_Mixin.java

---
+++ src/main/java/com/science/gtnl/mixins/early/render/RendererLivingEntity_Mixin.java
@@ -0,0 +1,27 @@
+package com.science.gtnl.mixins.early.render;
+
+import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.EntityLivingBase;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.science.gtnl.common.item.TimeStopManager;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SuppressWarnings("UnusedMixin")
+@Mixin(RendererLivingEntity.class)
+public abstract class RendererLivingEntity_Mixin {
+
+    /*@Inject(method = "rotateCorpse(Lnet/minecraft/entity/EntityLivingBase;FFF)V", at = @At("HEAD"), cancellable = true)
+    private void onRotateCorpse(EntityLivingBase par1EntityLivingBase, float par2, float par3, float par4,
+        CallbackInfo ci) {
+        if (TimeStopManager.isTimeStopped()) {
+            ci.cancel();
+        }
+    }*/
+}
